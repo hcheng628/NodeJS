6902cbea8978527272b11b2d232346b72b1ede32
superhcheng <chenghongyu628@gmail.com>
2017-02-25 20:15:51 -0500

    create todo-api-app

---
+++ node-execs/todo-api/playground/mongodb.util.js
@@ -0,0 +1,40 @@
+var mongoClient = require('mongodb').MongoClient;
+var mongoObjectID = require('mongodb').ObjectID;
+
+var assert = require('assert');
+
+var url_A = 'mongodb://159.203.206.215:27017/';
+var url_B = 'mongodb://159.203.206.217:27017/';
+var url_C = 'mongodb://159.203.206.219:27017/';
+var url_D = 'mongodb://159.203.206.221:27017/';
+var url_E = 'mongodb://159.203.206.223:27017/';
+var url_F = 'mongodb://159.203.206.225:27017/';
+
+var mongodbProjName = 'ToDoApp';
+
+mongoClient.connect(url_A + mongodbProjName, (err, db) =>{
+    assert.equal(null, err);
+    console.log("Connected :-)");
+
+    // db.collection('Computers').insertOne({
+    //     "brand": "Apple",
+    //     "name": "MacBook Pro"
+    // }, (err, res)=>{
+    //     assert.equal(null,err);
+    //     console.log(JSON.stringify(res.ops,undefined,2));
+    // });
+    var objID = new mongoObjectID('58b220f3f078d424af9461bb');
+    db.collection('Computers').find({_id: objID }).toArray().then((doc)=>{
+        console.log('doc: ' + JSON.stringify(doc,undefined,2));
+    },(err)=>{
+        console.log('err', err);
+    });
+
+    db.collection('Computers').find().count().then((count)=>{
+        console.log('count: ' + count);
+    },(err)=>{
+        console.log('err', err);
+    });
+
+    db.close();
+});
